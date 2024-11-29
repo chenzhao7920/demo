3d80e2261258ba2066eedb0dd7d4528f5828366b
chenzhao7920 <chenzhao7920@gmail.com>
2024-11-29 17:12:51 -0500

    add constrain to locations table

--- server/src/controllers/batchCreateLocationsFromCSV.ts
+++ server/src/controllers/batchCreateLocationsFromCSV.ts
@@ -5,50 +5,129 @@ import { Readable } from 'stream';
 const prisma = new PrismaClient();
 export const batchCreateLocationsFromCSV = async(req: any, res: any) =>{
     const { file } = req
-    if(file?.mimetype !== 'text/csv'){
-        res.status(400).json({message: "not valid csv file"});
-    }
-    if(file.size > 500 * 1024){
-        res.status(400).json({message: "not valid file size, must less then 500KB"});
-    }
-
     const job_queue: any = []
-    const BATCH_LIMIT = 5
+    const BATCH_LIMIT = 20
     try{
         const stream = Readable.from(file.buffer)
         stream.pipe(parse({headers: true,  discardUnmappedColumns: true, ignoreEmpty: true, trim: true}))
-            .on("data", async (row) => {
+            .on("data", async(row) => {
+                //console.log("data",row)
                 job_queue.push(row)
                 if(job_queue.length >= BATCH_LIMIT){
                     let jobs = job_queue.splice(0,BATCH_LIMIT)
-                    await createManyLocations(jobs)
+                    setTimeout(async()=>{
+                        await createManyLocations(jobs);
+                    },500)
                 }
             })
             .once("end", async()=>{
-                console.log("on end")
                 if(job_queue.length){
                     await createManyLocations(job_queue);
                 }
-                // await profileQueue.add("end", {}, { delay: 2000, removeOnComplete: true, removeOnFail: true });
-                //return resolve({ message: "File is read successfully, batch creation request is submitted", data });
             })
             .once("error", (err)=>{
-                console.error("Error processing CSV:", err);
-               //return reject(new Error(error.message));
+                res.status(400).json({message: `Error processing CSV: ${file.originalname}, ${err}`})
             });
-            return res.json({message: `We are working on loading data from ${file.originalname}`})
+            return res.status(200).json({message: `We are working on loading data from ${file.originalname}`})
     }catch(err){
         return res.json({message: file.originalname})
     }
 }
-const createManyLocations = async(rows:any) =>{
-   // let {street, city, zip_code, county, latitude, longtitude, time_zone}  =
-    let promises = []
-    rows.forEach((row:any)=>{
-        promises.push()
-    })
-    setTimeout(()=>{
-        console.log("5 line data",rows)
-    }, 1000)
-   // await locations.create
+const createManyLocations = async(locations:any) =>{
+    try{
+        const fail_created:any = []
+        prisma.$transaction(async(tx:any) =>{
+            const cities = await Promise.allSettled(
+                locations.map((location: any) => {
+                    return tx.cities.upsert({
+                        create:{name: location.city},
+                        where:{name: location.city},
+                        update:{name: location.city,updatedAt: new Date()},
+                    })
+                })
+            )
+            const counties = await Promise.allSettled(
+                locations.map((location: any)=>{
+                    return tx.counties.upsert({
+                        create:{name: location.county},
+                        where:{name: location.county},
+                        update:{name: location.county,updatedAt: new Date()}
+                    })
+                })
+            )
+            const countries = await Promise.allSettled(
+                locations.map((location:any)=>{
+                    return tx.countries.upsert({
+                        create:{name: location.country},
+                        where:{name: location.country},
+                        update:{name: location.country,updatedAt: new Date()}
+                    })
+                })
+            )
+
+            const timezones = await Promise.allSettled(
+                locations.map((location:any)=>{
+                    return tx.timezones.upsert({
+                        create:{name: location.time_zone},
+                        where:{name: location.time_zone},
+                        update:{name: location.time_zone,updatedAt: new Date()},
+
+                    })
+                })
+            )
+
+            const locationsToCreate = locations.map((location:any, idx: number) =>{
+                if(counties[idx].status === 'fulfilled' &&
+                    cities[idx].status === 'fulfilled' &&
+                    countries[idx].status === 'fulfilled' &&
+                    timezones[idx].status === 'fulfilled'
+                )
+                return {
+                    street: location.street,
+                    zip_code: location.zip_code,
+                    latitude: parseFloat(location.latitude),
+                    longitude: parseFloat(location.longitude),
+                    city_id: cities[idx].value.id,
+                    county_id: counties[idx].value.id,
+                    country_id: countries[idx].value.id,
+                    timezone_id: timezones[idx].value.id
+                }
+                if(counties[idx].status === 'rejected' ||
+                    cities[idx].status === 'rejected' ||
+                    countries[idx].status === 'rejected' ||
+                    timezones[idx].status === 'rejected'
+                    ){
+                       fail_created.push(location)
+                    }
+            })
+
+            //await tx.locations.createMany({data: locationsToCreate})
+            const created_locations = await Promise.allSettled(
+                locationsToCreate.map((location:any)=>{
+                    return tx.locations.upsert({
+                        where:{ street_latitude_longitude: {
+                            street: location?.street,
+                            latitude: location?.latitude,
+                            longitude: location?.longitude
+                            }},
+                        update:{
+                            street: location?.street,
+                            latitude: location?.latitude,
+                            longitude: location?.longitude,
+                            updatedAt: new Date()
+                        },
+                        create:{...location}
+                    })
+                })
+            )
+            console.log(fail_created)
+            return created_locations
+        },{
+            timeout: 30000 // Increase timeout to 30 seconds (default is 5 seconds)
+        })
+
+    }catch(err){
+        console.log(err)
+        throw(err)
+    }
 }
