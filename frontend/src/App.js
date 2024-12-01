249ca2ecb31dda028d2e3c9399382e9791f8cec6
chenzhao7920 <chenzhao7920@gmail.com>
2024-11-30 21:16:26 -0500

    add mui and UI dashboard from template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import GeoLocations from "./pages/geoLocations"
+import NotFound from "./components/NotFound"
 function App() {
   return (
      <Router>
         <Routes>
-          <Route path = '/' element = {<>"Hello world"</>}/>
-          <Route path = '/dashboard' element = {<>"Hello Dashboard"</>}/>
+          <Route path = '/' element = {<Navigate to="/locations" replace />}/>
+          <Route path = '/locations' element = {<GeoLocations/>}/>
+          <Route path="*" element={<NotFound />} />
         </Routes>
      </Router>
   );
